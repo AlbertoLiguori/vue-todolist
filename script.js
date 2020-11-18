e7c2ae4b398bd416cdc55e7c0baaaf7bc7a9891b
Alberto Liguori <a1liguorialberto@gmail.com>
2020-11-18 17:04:11 +0100

    changing strategy

--- script.js
+++ script.js
@@ -3,22 +3,75 @@ const myApp = new Vue({
 
   el:("#root"),
 
-  data:{
-    newTodo:"",
-    todoArray: [],
-  },
+    data:{
 
-  methods:{
-    getInput: function(){
-      //console.log(this.newTodo);
-      this.todoArray.push(this.newTodo);
-      //console.log(this.todoArray);
-      this.newTodo="";
+      inputText:"",
 
+      todoArray:[],
     },
 
-    removeTodo: function(index,howMany){
-      this.todoArray.splice(index,howMany)
+    methods:{
+
+      addTodo: function(){
+
+        let todoObject = {
+          todoText:this.inputText,
+          backgroundColor:"blue",
+        };
+
+        this.todoArray.push(todoObject);
+        console.log(todoObject.todoText);
+        console.log(this.todoArray);
+
+        this.inputText = "";
+      },
+
+      removeTodo: function(index,howMany){
+          this.todoArray.splice(index,howMany);
+      },
+
+      changeColor: function(index){
+        if (this.todoArray[index].backgroundColor == "blue"){
+          this.todoArray[index].backgroundColor = "coral"
+        }else{
+          this.todoArray[index].backgroundColor = "blue"
+        };
+      },
+
+
+
     }
-  }
+
+
+
 })
+
+
+// data:{
+//   newTodo: "",
+//   todoArray: [],
+//   actualColor: "blue",
+//   colorRed: "coral",
+//
+// },
+//
+// methods:{
+//
+//   getInput: function(){
+//
+//     this.todoArray.push(this.newTodo);
+//     this.newTodo = "";
+//   },
+//
+//   removeTodo: function(index,howMany){
+//     this.todoArray.splice(index,howMany);
+//   },
+//
+//   changeColor: function(){
+//     if (this.actualColor == "blue"){
+//       this.actualColor = this.colorRed
+//     }else{
+//       this.actualColor = "blue"
+//     };
+//   },
+// },
